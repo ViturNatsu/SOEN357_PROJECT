88619c05b592c85324b859a6a1ed91b7f32df977
alexhakim <alexandre.hakim@mail.concordia.ca>
2024-03-25 10:52:55 -0400

    Initial Layout Appointment/Reminder

--- app/src/main/java/com/example/soen357_project/AppointmentsAndReminders.java
+++ app/src/main/java/com/example/soen357_project/AppointmentsAndReminders.java
@@ -5,12 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AppointmentsAndReminders extends AppCompatActivity {
+
+    private FloatingActionButton addAppointmentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointments_and_reminders);
+
+        addAppointmentButton = findViewById(R.id.addAppointmentButton);
+        addAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AppointmentsAndReminders.this, add_appointments_and_reminders.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onImageClicked(View view) {
@@ -19,10 +32,4 @@ public class AppointmentsAndReminders extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(AppointmentsAndReminders.this, Dashboard.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/example/soen357_project/add_appointments_and_reminders.java
+++ app/src/main/java/com/example/soen357_project/add_appointments_and_reminders.java
@@ -1,7 +1,10 @@
 package com.example.soen357_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class add_appointments_and_reminders extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class add_appointments_and_reminders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointments_and_reminders);
+    }
+
+    public void onImageClicked(View view) {
+        Intent intent = new Intent(add_appointments_and_reminders.this, AppointmentsAndReminders.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
     }
 }
